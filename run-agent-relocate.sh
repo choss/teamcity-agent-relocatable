5775d5a154d614f104b6472c1664e1bb789148f5
choss <clayton.hoss@googlemail.com>
2020-04-11 14:30:37 +0200

    Update run-agent-relocate.sh

--- run-agent-relocate.sh
+++ run-agent-relocate.sh
@@ -4,7 +4,7 @@ then
   echo "Starting with default"
 else
   echo "Starting to relocate to ${RELOCATE_DIR}"
-  ln -s ${RELOCATE_DIR} /opt/buildagent
+  ln -nsf ${RELOCATE_DIR} /opt/buildagent
   sed -i "s|^AGENT_DIST=.*$|AGENT_DIST=${RELOCATE_DIR}|g" /run-agent.sh
   sed -i "s|^LOG_DIR=.*$|LOG_DIR=${RELOCATE_DIR}/logs|g" /run-agent.sh
 fi
