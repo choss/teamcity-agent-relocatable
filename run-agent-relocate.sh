9a5ec00f18beb40658e975e90a8676ae04493304
choss <clayton.hoss@googlemail.com>
2020-04-11 16:13:56 +0200

    Update run-agent-relocate.sh

--- run-agent-relocate.sh
+++ run-agent-relocate.sh
@@ -5,7 +5,7 @@ then
 else
   echo "Starting to relocate to ${RELOCATE_DIR}"
   mkdir -p `dirname ${RELOCATE_DIR}`
-  cp -rf /opt/buildagent ${RELOCATE_DIR}
+  cp -rfv /opt/buildagent ${RELOCATE_DIR}
   sed -i "s|^AGENT_DIST=.*$|AGENT_DIST=${RELOCATE_DIR}|g" /run-agent.sh
   sed -i "s|^LOG_DIR=.*$|LOG_DIR=${RELOCATE_DIR}/logs|g" /run-agent.sh
 fi
